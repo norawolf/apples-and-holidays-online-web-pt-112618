6a0e2af86cc5c3b0813b2fbd60077e2fd65676e8
Nora Vogt <noraevogt@gmail.com>
2018-12-14 17:25:33 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -23,10 +23,6 @@ def second_supply_for_fourth_of_july(holiday_hash)
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
-  # holiday_hash is identical to the one above
-  # add the second argument, which is a supply, to BOTH the
-  # Christmas AND the New Year's arrays
-
   holiday_hash[:winter].each do |holiday, supplies|
     supplies << supply
   end
@@ -34,36 +30,18 @@ end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
-  # again, holiday_hash is the same as the ones above
-  # add the second argument to the memorial day array
-
   holiday_hash[:spring][:memorial_day] << supply
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  # code here
-  # remember to return the updated hash
   holiday_hash[season][holiday_name] = supply_array
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-  # return an array of all of the supplies that are used in the winter season
-  winter_supplies = []
-  holiday_hash[:winter].each do |holiday, supplies|
-    winter_supplies << supplies
-  end
-  winter_supplies.flatten
+  holiday_hash[:winter].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
-  # iterate through holiday_hash and print items such that your readout resembles:
-  # Winter:
-  #   Christmas: Lights, Wreath
-  #   New Years: Party Hats
-  # Summer:
-  #   Fourth Of July: Fireworks, BBQ
-  # etc.
-  print_list = ""
   holiday_hash.each do |season, hol_name_and_supply_hash|
     puts "#{season.to_s.capitalize}:"
     hol_name_and_supply_hash.each do |holiday, supplies|
@@ -73,24 +51,15 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
-
-# original solution
-#  bbq_holidays = []
-
-#  holiday_hash.each do |season, hol_name_and_supply_hash|
-#    hol_name_and_supply_hash.each do |holiday, supplies|
-#      bbq_holidays << holiday if supplies.include?("BBQ")
-#    end
-#  end
-#  bbq_holidays
-
-# refactoring
   holiday_hash.map do |season, hol_name_and_supply_hash|
     hol_name_and_supply_hash.map do |holiday, supplies|
       holiday if supplies.include?("BBQ")
     end
   end.flatten.compact
+
+
+
+# flatten being called on return value of iterator. chaining on "end" is on return value of the loop
+#why can we chain methods to "end" here?
 
 end
